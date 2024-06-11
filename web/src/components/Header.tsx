2345bddb7ea73f1fcdbbbbb0003a21c9c453acf5
davipadua <davipadua1@gmail.com>
2024-06-11 15:48:16 -0300

    responsive header

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,79 +1,69 @@
 import React from "react";
-import { ReactNode } from "react";
-import styled from "styled-components"
-import LogoImg from "../assets/logo-img.png"
+import styled from "styled-components";
+import LogoImg from "../assets/logo-img.png";
 import { Link } from "react-router-dom";
-
-
 
 const StyledContainer = styled.div`
     background-color: #9EC09F;
-    margin: 0;
     display: flex;
     flex-direction: column;
-    justify-content: center;
     align-items: center;
-    padding-top: 10px
+    padding-top: 10px;
 `;
 
 const StyledImage = styled.img`
     width: 150px;
-    padding: 0px;
 `;
 
 const StyledTitle = styled.h1`
     color: white;
-    margin: 0;
-    font-size: 50px;
+    font-size: 40px;
     font-family: Roboto;
     font-weight: 400;
+    margin: 10px 0 25px;
 `;
 
 const StyledNavBar = styled.div`
     border-bottom: 1px solid black;
-    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2); /* Adicionando sombra */
-    width: auto;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+    width: 100%;
     background-color: white;
-    padding: 15px 100%;
-    margin: 0;
     display: flex;
-`
+    justify-content: center;
+`;
+
 const StyledMenuItems = styled(Link)`
-    display: inline-block;
-    padding: 4px 0;
-    width: 175px;
+    display: block;
+    padding: 10px 5px;
     text-align: center;
     font-family: Roboto;
     text-decoration: none;
     color: black;
     transition: 0.4s ease;
+    flex: 1;
 
     &:hover {
-        
         color: #9EC09F;
-      }
-`
+    }
 
-// interface ContainerProps {
-//     children: ReactNode;
-// }
+    @media (max-width: 400px) {
+        font-size: 13px;
+        padding-top: 10px;
+    }
+`;
 
 export default function Container() {
     return (
         <StyledContainer>
-            <StyledImage src={LogoImg}/>
-
+            <StyledImage src={LogoImg} alt="Logo" />
             <StyledTitle>Turtle’s Cake</StyledTitle>
-
             <StyledNavBar>
                 <StyledMenuItems to={"/"}>Bolos</StyledMenuItems>
                 <StyledMenuItems to={"/tortas"}>Tortas</StyledMenuItems>
-                <StyledMenuItems to={"/sobremesas"}>Doces</StyledMenuItems>
+                <StyledMenuItems to={"/sobremesas"}>Sobremesas</StyledMenuItems>
                 <StyledMenuItems to={"/fidelidade"}>Fidelidade</StyledMenuItems>
                 <StyledMenuItems to={"/sobre-nos"}>Sobre Nós</StyledMenuItems>
-
             </StyledNavBar>
-            
         </StyledContainer>
-    )
+    );
 }
